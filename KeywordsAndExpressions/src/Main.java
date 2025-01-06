4356ab7fa0226c0266661458fd8288c2c9d29297
SevdeSalkim <sevde.salkim@gmail.com>
2025-01-06 22:51:35 +0300

    KeywordsAndExpressions düzenlendi

--- KeywordsAndExpressions/src/Main.java
+++ KeywordsAndExpressions/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
+        int highScore = 50;
+        if (highScore > 25){
+            highScore = highScore + 1000;
+            System.out.println(highScore);
         }
+
+        int health = 100;
+
+        if((health < 120) && (highScore > 1000)){
+            highScore = highScore - 1000;
+            System.out.println(highScore);
+        }
+        /* Bu Java programı, anahtar kelimeler ve mantıksal ifadelerin kullanımını göstermektedir.
+         Programda değişkenler, koşullu ifadeler ve mantıksal operatörler kullanılarak belirli koşullara dayalı basit aritmetik işlemler yapılmaktadır.*/
+    }
 }
