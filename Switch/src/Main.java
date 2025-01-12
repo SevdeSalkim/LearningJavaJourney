27693b55f2664ef708fc559f422aa29cd161016a
SevdeSalkim <sevde.salkim@gmail.com>
2025-01-12 22:24:42 +0300

    Add Switch

---
+++ Switch/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int value = 3;
+
+        switch (value){
+            case 1 :
+                System.out.println("Value was 1");
+                break;
+            case 2 :
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+    }
+}
