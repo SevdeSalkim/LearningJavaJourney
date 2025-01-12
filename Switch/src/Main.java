a13f3e3aced4f1c31910c851d458caea24ef60e7
SevdeSalkim <sevde.salkim@gmail.com>
2025-01-13 01:47:46 +0300

    Switch edited

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int value = 3;
 
+        //Traditional Switch Statement
+        /*
         switch (value){
             case 1 :
                 System.out.println("Value was 1");
@@ -16,6 +18,44 @@ public class Main {
             default:
                 System.out.println("Was not 1 , 2, 3, 4, or 5");
                 break;
+        }*/
+
+        // Enhanced Switch Statement
+        switch (value) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+                System.out.println("Was a 3, a 4, or a 5");
+                System.out.println("Actually it was a " + value);
+            }
+            default -> System.out.println("Was not 1 , 2, 3, 4, or 5");
         }
+        // Valid Switch Value Type:
+        // Byte,short,int,char
+        // Cannot use long,float,double or boolean or their wrappers
+
+        System.out.println(getQuarter("December"));
+    }
+
+    public static  String getQuarter(String month){
+        switch (month){
+            case "January":
+            case "February":
+            case "March":
+                return "1st";
+            case "April":
+            case "May":
+            case "June":
+                return "2nd";
+            case "July":
+            case "Augst":
+            case "September":
+                return "3rd";
+            case "October":
+            case "November":
+            case "December":
+                return "4th";
+        }
+        return "bad";
     }
 }
